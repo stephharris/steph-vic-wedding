227b92cd84b6d597fbcbb7f8eeaaa36f17b636ff
Steph Harris <stephanie.harris@firstlook.org>
2020-04-25 14:48:24 -0400

    adding more to the gitignore

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,11 @@
 import withLayout from '../hocs/withLayout';
+import Image from '../components/Image';
 import '../styles/index.css';
 
 const About = () => (
   <div className="p-12 sm:p-8">
     This is the about page.
+    <Image />
   </div>
 )
 
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import withLayout from '../hocs/withLayout';
 import HomepageTitle from '../components/HomepageTitle';
+import CrosswordInput from '../components/CrosswordInput';
 import '../styles/index.css';
 
 const Homepage = (props) => {
   return (
-    <div className="w-screen h-screen absolute -z-1 top-0">
+    <div className="w-screen h-screen">
       <HomepageTitle />
+      <CrosswordInput />
     </div>
   )
 }
