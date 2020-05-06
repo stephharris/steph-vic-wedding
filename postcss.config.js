81c09a156c862377bf7d156a8fb13ab2a02ebb25
Steph Harris <stephanie.harris@firstlook.org>
2020-05-06 09:49:05 -0400

    reconfigured configs

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,21 @@
 module.exports = {
-  plugins: ['tailwindcss', 'autoprefixer', 'postcss-preset-env'],
+  plugins: [
+    'tailwindcss',
+    ...(process.env.NODE_ENV === 'production'
+      ? [
+          [
+            '@fullhuman/postcss-purgecss',
+            {
+              content: [
+                './pages/**/*.{js,jsx,ts,tsx}',
+                './components/**/*.{js,jsx,ts,tsx}',
+              ],
+              defaultExtractor: content =>
+                content.match(/[\w-/:]+(?<!:)/g) || [],
+            },
+          ],
+        ]
+      : []),
+    'postcss-preset-env',
+  ],
 }
