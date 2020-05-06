73cede9f71880524a58264048aa182235d7feeed
Steph Harris <stephanie.harris@firstlook.org>
2020-05-06 15:31:45 -0400

    crossword working

--- next.config.js
+++ next.config.js
@@ -4,12 +4,5 @@ const withPlugins = require('next-compose-plugins')
 module.exports = withPlugins(
   [
     withImages,
-  ],
-  {
-    env: {
-      SECRET_CODE: process.env.SECRET_CODE,
-      SECRET_VIDEO: process.env.SECRET_VIDEO,
-      SECRET_SUCCESS_MSG: process.env.SECRET_SUCCESS_MSG
-    }
-  },
+  ]
 );
--- pages/api/crossword.js
+++ pages/api/crossword.js
@@ -4,7 +4,7 @@ export default (req, res) => {
   const secretUrl = process.env.SECRET_VIDEO
   const secretSuccessMsg = process.env.SECRET_SUCCESS_MSG
 
-  if(req.body.data === secret) {
+  if(data === secret) {
     res.status(200).json({ secret: secret, successMessage: secretSuccessMsg, link: secretUrl, isCorrect: true })
   } else {
     res.status(200).json({ isCorrect: false })
